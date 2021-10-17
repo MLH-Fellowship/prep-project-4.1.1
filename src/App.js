b66f99f904e499d1176807f5025a2f400617ceba
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-10-17 23:31:52 +0530

    merged conflicts

--- src/App.js
+++ src/App.js
@@ -46,23 +46,65 @@ function App() {
             value={city}
             onChange={(event) => setCity(event.target.value)}
           />
-          <div className="Results">
-            {!isLoaded && <h2>Loading...</h2>}
-            {console.log(results)}
-            {isLoaded && results && (
-              <>
-                <h3>{results.weather[0].main}</h3>
-                <p>Feels like {results.main.feels_like}°C</p>
-                <i>
-                  <p>
-                    {results.name}, {results.sys.country}
-                  </p>
-                </i>
-              </>
-            )}
+          <div className="Result_card">
+            <div className="Results">
+              {!isLoaded && <h2>Loading...</h2>}
+              {console.log(results)}
+              {isLoaded && results && (
+                <>
+                  <h3>{results.weather[0].main}</h3>
+                  <p>Feels like {results.main.feels_like}°C</p>
+                  <i>
+                    <p>
+                      {results.name}, {results.sys.country}
+                    </p>
+                  </i>
+                </>
+              )}
+            </div>
           </div>
 
-{/* Tip Div */} {isLoaded && results && ( <div className="tip-div"> <> <h2> Tip! </h2> { (results.weather[0].main === "Rain" || results.weather[0].main === "Clouds" ) && ( <h3> Bring an Umbrella, it might get wet! <img src="umbrella.png" alt="Umbrella" className="tip-img" /> </h3> )} {results.weather[0].main === "Snow" && ( <h3> Bring your coat, it might get chilly! <img src="coat.png" alt="Coat" className="tip-img" /> </h3> )} {results.weather[0].main === "Clear" && ( <h3> Bring your suncream, so that tan is good!{" "} <img src="suncream.png" alt="Sun Cream" className="tip-img" /> </h3> )} {results.wind.speed >= 2.0 && ( <h3> Bring your wind-cheater, lest you want wind bites! <img src="jacket.png" alt="Jacket" className="tip-img" /> </h3> )} </> </div> )} </div>
+          {/* Tip Div */}
+          {isLoaded && results && (
+            <div className="tip-div">
+              <>
+                <h2>Tip!</h2>
+                {(results.weather[0].main === "Rain" ||
+                  results.weather[0].main === "Clouds") && (
+                  <h3>
+                    Bring an Umbrella, it might get wet!
+                    <img
+                      src="umbrella.png"
+                      alt="Umbrella"
+                      className="tip-img"
+                    />
+                  </h3>
+                )}
+                {results.weather[0].main === "Snow" && (
+                  <h3>
+                    Bring your coat, it might get chilly!
+                    <img src="coat.png" alt="Coat" className="tip-img" />
+                  </h3>
+                )}
+                {results.weather[0].main === "Clear" && (
+                  <h3>
+                    Bring your suncream, so that tan is good!{" "}
+                    <img
+                      src="suncream.png"
+                      alt="Sun Cream"
+                      className="tip-img"
+                    />
+                  </h3>
+                )}
+                {results.wind.speed >= 2.0 && (
+                  <h3>
+                    Bring your wind-cheater, lest you want wind bites!
+                    <img src="jacket.png" alt="Jacket" className="tip-img" />
+                  </h3>
+                )}
+              </>
+            </div>
+          )}
         </div>
       </>
     );
