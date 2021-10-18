d7626090089e63280c98aa973dd63f4ea79c086c
atharmohammad <mohd.rule123@gmail.com>
2021-10-18 22:29:41 +0530

    notification componenet added

---
+++ src/Components/Notifications/Notifications.js
@@ -0,0 +1,8 @@
+
+export default function Notify(props){
+    return(
+        <div
+        style={{padding:'20px',background:'orange',width:'90%',margin:'auto',color:'white'}}
+        >{props.notification}</div>
+    )
+}
